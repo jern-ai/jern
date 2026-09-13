756e853d51f92f6b97d95cc3ff2e606757181e4f
Ademar Gonzalez <ademar.gonzalez@gmail.com>
2026-09-12 23:57:24 -0400

    Clean up test parallelization settings
    Removed unused test collection definition and parallelization settings.

--- tests/Jern.Tests/Parallelism.fs
+++ tests/Jern.Tests/Parallelism.fs
@@ -8,8 +8,4 @@ module Jern.Tests.Parallelism
 open Xunit
 
 [<assembly: CollectionBehavior(DisableTestParallelization = true)>]
-
-[<CollectionDefinition("Process state", DisableParallelization = true)>]
-type ProcessStateCollection() = class end
-
 do ()
